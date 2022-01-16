731b8cbd80014dd968140fa490ba0c627f263e2f
Artyom Kim <artem_kim_2006@list.ru>
2022-01-16 11:34:26 +0600

    implemented firebase utlis

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShopPage from './pages/Shop/shop.component'
 import HomePage from './pages/Homepage/homepage.component'
 import Header from './components/Header/header.component';
 import SignInAndSignUpPage from './pages/Sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import React from 'react';
 
 
@@ -25,12 +25,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user)
-    })
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
+
 
   componentWillUnmount() {
     this.unsubscribeFromAuth()
--- src/pages/Sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/Sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignIn from '../../components/Sign-in/sign-in.component';
+import SignUp from '../../components/Sign-up/sign-up.component';
 import styles from './sign-in-and-sign-up.module.scss'
+
 
 const SignInAndSignUpPage = () => {
 	return (
 		<div className={styles.signInAndSignUpPage}>
 			<SignIn />
+			<SignUp />
 		</div>
 	)
 }
